6dd2cddc5a4321569dc2f0e364bd97c8e48867e5
TheusZer0 <quetzal.noc@gmail.com>
2021-10-31 04:08:25 -0300

    falta imprimir lo ultimo y esta listo, se cambio y ahora el indice 0 = 0 lo que implica que el indice 10 es el 55

--- Parte_B.c
+++ Parte_B.c
@@ -16,7 +16,7 @@ int Fib_Array [LENGTH];
 
 int validacion(int value);
 
-void* get_Fibonacci_series(int*);
+void* get_Fibonacci_series(void*);
 
 int main(int argc, char **argv) {
     int *NumFibonacci = NULL;
@@ -24,53 +24,49 @@ int main(int argc, char **argv) {
     pthread_t tid; /* the thread identifier */
     pthread_attr_t attr; /* set of thread attributes */
 
-    if (argc==2) {
-        int a = (atoi(argv[1]));//hace referencia al valor que se ingresa por comando
-        int validator = validacion(a);
-
-        if(a<=LENGTH){
-            NumFibonacci = &a;
-
-            //Cumple la condicion
-
-            /* get the default attributes */
-            pthread_attr_init(&attr);
-            /* create the thread */
-            pthread_create(&tid, &attr, get_Fibonacci_series(NumFibonacci), argv[1]);
-            /* wait for the thread to exit */
-            pthread_join(tid,NULL);
-
-            for (int i = 0; i <= LENGTH; ++i) {
-                printf("The number is: %d \n",Fib_Array[i]);
-            }
-
-        }else{
-            exit(-1);
-        }
-
-    } else{ //caso contrario, si no cumple retorna 1
+    if (argc != 2) {
         printf("El programa funciona como ParteB.out <Numero>\n");
         return EXIT_FAILURE;
     }
+
+    if ((atoi(argv[1]) >= LENGTH) | ((atoi(argv[1]) < 0)) ){
+        printf("Ta malo\n");
+        return EXIT_FAILURE;
+    }
+
+    int a = (atoi(argv[1]));//hace referencia al valor que se ingresa por comando
+    int validator = validacion(a);
+    NumFibonacci = &a;
+    //Cumple la condicion
+    /* get the default attributes */
+    pthread_attr_init(&attr);
+    /* create the thread */
+    pthread_create(&tid, &attr, get_Fibonacci_series, argv[1]);
+    /* wait for the thread to exit */
+    pthread_join(tid,NULL);
+    for (int i = 0; i <= (atoi(argv[1])); ++i) {
+        printf("Los numeros almacenados corresponden a: %d \n",Fib_Array[i]);
+    }
+
 }
 
 /* The thread will begin control in this function */
-void* get_Fibonacci_series(int* numb){
+void* get_Fibonacci_series(void* param){
 
     int i;
-
+    int numb = atoi(param);
     // initialize first and second terms
     int t1 = 0, t2 = 1;
     Fib_Array[0] = t1;
     // initialize the next term (3rd term)
     int nextTerm = t1 + t2;
 
-    if(*numb > 1){
+    if(numb > 1){
         Fib_Array[1] = 1;
     }
 
     // print 3rd to nth terms
-    for (i = 2; i <= *numb; ++i) {
+    for (i = 2; i <= numb; ++i) {
         t1 = t2;
         t2 = nextTerm;
         Fib_Array[i] = nextTerm;
@@ -78,7 +74,7 @@ void* get_Fibonacci_series(int* numb){
     }
 
     // Thread ID: #### calculó los ## números de la serie Fibonacci
-    printf("Thread ID = %lu calculó los %d números de la serie Fibonacci\n", pthread_self(),*numb);
+    printf("Thread ID = %lu calculó los %d números de la serie Fibonacci\n", pthread_self(),numb);
     pthread_exit(0);
 }
 
